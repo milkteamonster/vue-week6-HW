ae62b530957c07b2c099cb2a8e7f63401db8a45f
milkteamonster <milkteamonster@gmail.com>
2024-02-26 00:42:08 +0800

    Updates

---
+++ assets/CartView-cUdGEUyJ.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o}from"./index-4vq8JFo6.js";const r={};function s(a,c,n,p,_,i){return o(),t("h1",null,"This is Cart page.")}const l=e(r,[["render",s]]);export{l as default};
---
+++ assets/ProductsView-EtGZTMpT.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as s}from"./index-4vq8JFo6.js";const t={};function c(r,n,a,p,_,i){return s(),o("h1",null,"This is Products page.")}const d=e(t,[["render",c]]);export{d as default};
